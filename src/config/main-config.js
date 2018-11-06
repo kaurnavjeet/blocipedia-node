bfa532415108ca3c6d890464f67bc170cc2f7cbd
kaurnavjeet <kaurnavjeet09@gmail.com>
2018-11-05 18:56:55 -0800

    scaffold completed and deployed

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,5 +1,15 @@
 require("dotenv").config();
+const path = require("path");
+const viewsFolder = path.join(__dirname, "..", "views");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
 
 module.exports = {
-  init() {}
+  init(app, express) {
+    app.set("views", viewsFolder);
+    app.set("view engine", "ejs");
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(logger("dev"));
+    app.use(express.static(path.join(__dirname, "..", "assets")));
+  }
 };
