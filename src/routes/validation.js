9e7c2cc5babd7c92ef512b238e2c207d3fde6b3c
kaurnavjeet <kaurnavjeet09@gmail.com>
2018-11-06 12:48:44 -0800

    Fixed Validation errors

--- src/routes/validation.js
+++ src/routes/validation.js
@@ -2,7 +2,7 @@ module.exports = {
   validateUsers(req, res, next) {
     if (req.method === "POST") {
       req
-        .checkBody("username", "must be at least 6 characters in length")
+        .checkBody("username", "must be unique at least 6 characters in length")
         .isLength({ min: 6 });
       req.checkBody("email", "must be valid").isEmail();
       req
