61c4ef0ca9c8def9f38de8ac4c22b45cab709bdb
kaurnavjeet <kaurnavjeet09@gmail.com>
2018-11-06 02:44:32 -0800

    users signup POST WIP

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 const userController = require("../controllers/userController");
 
-router.post("/users/signup", userController.signup);
+router.get("/users/signup", userController.signUp);
 
 module.exports = router;
